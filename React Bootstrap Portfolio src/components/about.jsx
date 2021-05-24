1bfa864073f2e152d17d7d1349fb650d8dfdca90
Erik Čajagi <73696957+ecajagi@users.noreply.github.com>
2021-05-24 12:33:14 +0200

    portfolio

---
+++ React Bootstrap Portfolio src/components/about.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am an aspiring web-developer using mainly JavaScript. I am a proactive, diligent and quick to learn person, who is passionate about IT and all the life-improvement it brings to our lives. Coding is my hobby and joy and I devote all of my free time into becoming a developer. I am also well-spoken which makes me great at teamwork.</p>
+                    <p>I always inclined to tech and computers and since early age played with pascal, Adobe Flash and even had a school leaving exam from IT subject. Now, I have devoted my life to coding as it will certainly bring positive things to my life.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my skills</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I am abe to build websites using JavaScript, Express, React, HTML and CSS, where fron-end covers the back end as well.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures</h3>
+                    <p>At Amazon I assisted with creation of querries via SQL, helping create reports and analyse data more effectively.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>IT testing</h3>
+                    <p>Testing is a crutial part of coding and great gateway into SW development so I learned how to automate tests in Java.</p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
